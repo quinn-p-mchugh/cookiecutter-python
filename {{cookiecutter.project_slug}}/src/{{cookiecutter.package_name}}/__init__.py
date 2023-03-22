3aa41eb878bcff24ad4593c2bc61668a67edd38a
Quinn McHugh <qpmchugh@gmail.com>
2023-03-22 14:13:10 -0400

    Update src files

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for {{ cookiecutter.project_name }}."""
+
+__author__ = """{{ cookiecutter.full_name }}"""
+__email__ = '{{ cookiecutter.author_email }}'
+__version__ = '{{ cookiecutter.version }}'
---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -0,0 +1,7 @@
+"""Main module."""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
