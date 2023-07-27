32f5e11bfea7de15fbb27c5499dc0f52e1d08737
Quinn McHugh <qpmchugh@gmail.com>
2023-07-26 22:20:28 -0400

    Update gitignore

--- {{ cookiecutter.project_name }}/src/__main__.py
+++ {{ cookiecutter.project_name }}/src/__main__.py
@@ -5,11 +5,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
-{
-    "product_code": "nVSC Stack 38999",
-    "configuration_code": "P900 NA",
-    "factory": "0",
-    "line_number": "1",
-    "date": "2023‐07‐21T20:15:05Z",
-}
